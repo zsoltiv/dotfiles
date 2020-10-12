c811db7a763f2c263ea97bc0acb5e23fbfd63405
ShorTyy05 <vzsoltika05@gmail.com>
2020-10-12 19:57:53 +0200

    changed dwm font size

--- dwm/dwm-6.2.h
+++ dwm/dwm-6.2.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=9" };
+static const char dmenufont[]       = "monospace:size=9";
 static const char col_gray1[]       = "#0a0f14";
 static const char col_gray2[]       = "#bd93f9";
 static const char col_gray3[]       = "#edb54b";
