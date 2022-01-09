d1343887c08e6c58917b49b0b90ed13bd092b59c
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2022-01-09 19:49:07 +0100

    Improve backup script

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
 CONFIG_HOME="$HOME/.config"
+CONFIG_PATHS=(
+    "$HOME/.config"
+    "/etc"
+)
 DOTFILES_DIR="$(pwd)"
 
 paths=$(find . -not -path '\./.*' -not -path '\.' -not -executable -type f | sed "s/^\.\///")
+valid_config_paths=()
+for config_path in ${CONFIG_PATHS[@]}; do
+    if [[ -d $config_path ]]; then
+        valid_config_paths+=( $config_path )
+    fi
+done
 
+echo ${valid_config_paths[@]}
+
+# TODO: avoid copying the file from two different valid_config_paths
 for path in $paths; do
-    cp "$CONFIG_HOME/$path" "./$path" &> /dev/null &&
-        echo "copying $CONFIG_HOME/$path to ./$path"
+    for config_path in ${valid_config_paths[@]}; do
+        cp "$config_path/$path" "./$path" &> /dev/null &&
+            echo "copying $config_path/$path to ./$path" &&
+            break
+    done
 done
 
 git add .
